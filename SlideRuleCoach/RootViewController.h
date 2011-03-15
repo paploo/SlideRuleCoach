5c20f39d7c1e13c867c668138e92f57122aa0ecc
Jeff Reinecke <jeff@paploo.net>
2011-03-14 21:13:36 -0700

    Initial Commit

---
+++ SlideRuleCoach/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SlideRuleCoach
+//
+//  Created by Jeff Reinecke on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ SlideRuleCoach/SlideRuleCoachAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  SlideRuleCoachAppDelegate.h
+//  SlideRuleCoach
+//
+//  Created by Jeff Reinecke on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlideRuleCoachAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
