cfc1f98ae3480b78cd813f1c1c9520f9d411bef2
Jeff Reinecke <jeff@paploo.net>
2011-03-22 00:09:30 -0700

    Added proper license text.

--- SlideRuleCoach/MultiplicationProblemGenerator.h
+++ SlideRuleCoach/MultiplicationProblemGenerator.h
@@ -3,7 +3,7 @@
 //  SlideRuleCoach
 //
 //  Created by Jeff Reinecke on 3/17/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Jeffrey C Reinecke. Released under the BSD license.
 //
 
 #import <Foundation/Foundation.h>
