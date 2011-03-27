6443f8a6dce4ca35cb06ae0f437f4a358563ff41
Jeff Reinecke <jeff@paploo.net>
2011-03-27 16:28:47 -0700

    Scale Parameterization: Added trig scales.

--- SlideRuleCoach/ScaleParameterizer.m
+++ SlideRuleCoach/ScaleParameterizer.m
@@ -7,6 +7,7 @@
 //
 
 #import "ScaleParameterizer.h"
+#import "Trig.h"
 
 
 @implementation ScaleParameterizer
@@ -53,14 +54,35 @@
 
 // S and ST scale.
 + (double)sinScaleValueForParameter:(double)u {
-#warning Incomplete implementation.
-    return 0.0;
+    if(u <= 0.0) {
+        return [self mainScaleValueForParameter:u] * ONE_TENTH_RADIAN_IN_DEGREE;
+    } else if( (u >= 0.0) && u <=1.0) {
+        double s = [self mainScaleValueForParameter:u] / 10.0;
+        return asin(s) * DEGREE_TO_RADIAN;
+    } else {
+        NSException *e = [NSException exceptionWithName:NSInvalidArgumentException reason:[NSString stringWithFormat:@"Cannot calculate sin scale angle for parameter %f", u] userInfo:nil];
+        [e raise];
+        return 0.0;
+    }
 }
 
 // S and ST scale.
 + (double)sinScaleParameterForValue:(double)v {
-#warning Incomplete implementation.
-    return 0.0;
+    //First get the value (which is in degrees) into the appropriate range (0-90)
+    double value = asin(abs(sin(v*DEGREE_TO_RADIAN))) * RADIAN_TO_DEGREE;
+    
+    //Now we can calculate the paramaterized value.
+    if( (value > 0.0) && (value <= ONE_TENTH_RADIAN_IN_DEGREE) ) {
+        return [self mainScaleParameterForValue:(value / ONE_TENTH_RADIAN_IN_DEGREE)];
+    } else if( (value >= ONE_TENTH_RADIAN_IN_DEGREE) && (value <= 90.0) ) {
+        double s = sin(value*DEGREE_TO_RADIAN);
+        return [self mainScaleParameterForValue:(s*10.0)];
+    } else {
+        NSException *e = [NSException exceptionWithName:NSInvalidArgumentException reason:[NSString stringWithFormat:@"Cannot calculate sin scale parameter for angle %f", v] userInfo:nil];
+        [e raise];
+    }
+    
+    return 0.0; // This is to silence the compiler warnings.
 }
 
 + (double)cosScaleValueForParameter:(double)u {
@@ -72,16 +94,41 @@
 }
 
 // T and ST scale.
+// This returns values only under 45 degrees because of how the T scale folds.
 + (double)tanScaleValueForParameter:(double)u {
-    //This one is tricky because of how it reflects when over 45 degrees.
-#warning Incomplete implementation.
+    if( u <= 0.0 ) {
+        return [self mainScaleParameterForValue:u] * ONE_TENTH_RADIAN_IN_DEGREE;
+    } else if( (u >= 0.0) &&  (u <= 1.0) ) {
+        double t = [self mainScaleValueForParameter:u] / 10.0;
+        return atan(t) * DEGREE_TO_RADIAN;
+    } else {
+        NSException *e = [NSException exceptionWithName:NSInvalidArgumentException reason:[NSString stringWithFormat:@"Cannot calculate sin scale angle for parameter %f", u] userInfo:nil];
+        [e raise]; 
+    }
+            
     return 0.0;
 }
 
 // T and ST scale.
+// The T scale folds on top of itself for values over 45 degrees.
 + (double)tanScaleParameterForValue:(double)v {
-#warning Incomplete implementation.
-    return 0.0;
+    //First get the value (which is in degrees) into the appropriate range (0-90)
+    double value = atan(abs(tan(v*DEGREE_TO_RADIAN))) * RADIAN_TO_DEGREE;
+    
+    //Now we can calculate the paramaterized value.
+    if( (value > 0.0) && (value <= ONE_TENTH_RADIAN_IN_DEGREE) ) {
+        return [self mainScaleParameterForValue:(value / ONE_TENTH_RADIAN_IN_DEGREE)];
+    } else if( (value >= ONE_TENTH_RADIAN_IN_DEGREE) && (value <= 45.0) ) {
+        double t = sin(value*DEGREE_TO_RADIAN);
+        return [self mainScaleParameterForValue:(t*10.0)];
+    } else if( (value >= 45.0) && (value < 90.0) ) {
+        return [self tanScaleParameterForValue:(90.0 - v)];
+    } else {
+        NSException *e = [NSException exceptionWithName:NSInvalidArgumentException reason:[NSString stringWithFormat:@"Cannot calculate tan scale parameter for angle %f", v] userInfo:nil];
+        [e raise];
+    }
+    
+    return 0.0; // This is to silence the compiler warnings.
 }
 
 @end
