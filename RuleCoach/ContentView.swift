f6c3d39dba67d10e0fea9aaed014f32da6cf2c6b
Jeff Reinecke <jeff@paploo.net>
2020-07-05 13:01:43 -0700

    Framed out the root view for the app

--- RuleCoach/ContentView.swift
+++ RuleCoach/ContentView.swift
@@ -9,9 +9,108 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    @State var showSheet: Bool = false
+    @State var selectedMainNavItem: MainNavItem = .about
+    
+//    private var prefsButton: some View {
+//        Button(action: { self.showSettings.toggle()} ) {
+//        Image(systemName: "gear")
+//            .imageScale(.large)
+//            .padding()
+//        }
+//    }
+//
+//    private var helpButton: some View {
+//        Button(action: { self.showHelp.toggle()} ) {
+//        Image(systemName: "questionmark.circle")
+//            .imageScale(.large)
+//            .padding()
+//        }
+//    }
+    
+    private var sheetContents: some View {
+        switch self.selectedMainNavItem {
+        case .settings:
+            return Text("Settings")
+        case .about:
+            return Text("About")
+        }
     }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Group 1").bold()) {
+                    Text("Alpha")
+                    Text("Beta")
+                }
+                
+                Section(header: Text("Group 2").bold()) {
+                    Text("Alpha")
+                    Text("Beta")
+                    Image(systemName: "info.circle")
+                    Image(systemName: "questionmark.circle")
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle(Text("Exams"))
+            .navigationBarItems(trailing:
+                MainNavItemButtons(
+                    showSheet: $showSheet,
+                    selectedMainNavItem: $selectedMainNavItem
+                )
+            )
+//            .navigationBarItems(trailing: helpButton)
+//                .sheet(isPresented: $showHelp) {
+//                    Text("Help")
+//                }
+//            .navigationBarItems(trailing: prefsButton)
+            .sheet(isPresented: $showSheet) {
+                self.sheetContents
+            }
+        }
+    }
+    
+}
+
+struct MainNavItemButtons: View {
+    
+    @Binding var showSheet: Bool
+    @Binding var selectedMainNavItem: MainNavItem
+    
+    private var settingsButton: some View {
+        Button(action: {
+            self.selectedMainNavItem = MainNavItem.settings
+            self.showSheet.toggle()
+        } ) {
+        Image(systemName: "gear")
+            .imageScale(.large)
+        }
+    }
+    
+    private var aboutButton: some View {
+        Button(action: {
+            self.selectedMainNavItem = MainNavItem.about
+            self.showSheet.toggle()
+        } ) {
+        Image(systemName: "questionmark.circle")
+            .imageScale(.large)
+        }
+    }
+    
+    var body: some View {
+        HStack() {
+            aboutButton
+            Spacer().frame(width: 20.0)
+            settingsButton
+        }.padding()
+    }
+    
+}
+
+enum MainNavItem {
+    case settings, about
 }
 
 struct ContentView_Previews: PreviewProvider {
