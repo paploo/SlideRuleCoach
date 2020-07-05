4e1856e3e1f6555b6286a31a8a9da3486853e4d3
Jeff Reinecke <jeff@paploo.net>
2020-07-04 18:26:52 -0700

    Created a new Swift project to start from

---
+++ RuleCoach/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RuleCoach
+//
+//  Created by Jeff Reinecke on 7/4/20.
+//  Copyright © 2020 Jeff Reinecke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
